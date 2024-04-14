3e0bbda7a167ef23f9320ac755108cd50499662b
Vikash <vikashdas770@gmail.com>
2024-04-14 11:28:21 +0530

    Data Ingestion component added

--- template.py
+++ template.py
@@ -10,11 +10,13 @@ project_name = "cnnClassifier"
 list_of_files = [
     f"src/{project_name}/__init__.py",
     f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/config/__init__.py",
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
     "dvc.yaml",
